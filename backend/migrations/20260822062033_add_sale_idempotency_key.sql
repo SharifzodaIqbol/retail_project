aadbaad4323684f310d637c3437c6753133a5a3e
Шарифзода Икбол <iqbolsharifzoda724@gmail.com>
2026-08-22 12:16:31 +0500

    v1.3.4

--- backend/migrations/20260822062033_add_sale_idempotency_key.sql
+++ backend/migrations/20260822062033_add_sale_idempotency_key.sql
@@ -1,5 +1,17 @@
+-- +goose Up
+-- +goose StatementBegin
 ALTER TABLE sales ADD COLUMN IF NOT EXISTS idempotency_key TEXT;
 
 CREATE UNIQUE INDEX IF NOT EXISTS idx_sales_company_idempotency_key
     ON sales (company_id, idempotency_key)
     WHERE idempotency_key IS NOT NULL;
+
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+
+DROP INDEX IF EXISTS idx_sales_company_idempotency_key;
+ALTER TABLE sales DROP COLUMN IF EXISTS idempotency_key;
+
+-- +goose StatementEnd
